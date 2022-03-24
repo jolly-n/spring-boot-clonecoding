b25c2f6ca71a29d7fc1e9462f41c90081f0abf7f
yujeong lim <anneu5712@gmail.com>
2022-03-24 21:21:57 +0900

    게시판 조회, form 전송

--- src/main/java/com/godcoder/myhome/controller/BoardController.java
+++ src/main/java/com/godcoder/myhome/controller/BoardController.java
@@ -1,16 +1,59 @@
 package com.godcoder.myhome.controller;
 
+import com.godcoder.myhome.model.Board;
+import com.godcoder.myhome.repository.BoardRepository;
+import com.godcoder.myhome.validator.BoardValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardValidator boardValidator;
+
+    //게시판 목록
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+        List<Board> boards = boardRepository.findAll(); //전체 조회
+        model.addAttribute("boards", boards); //모델에 저장
         return "board/list";
+    }
+
+    //글쓰기 폼
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if (id == null) {
+            model.addAttribute("board", new Board());
+        } else {
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    //글쓰기 저장
+    @PostMapping("/form")
+    //public String formSubmit(@ModelAttribute Board board) {
+    public String formSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 
 }
---
+++ src/main/java/com/godcoder/myhome/model/Board.java
@@ -0,0 +1,22 @@
+package com.godcoder.myhome.model;
+
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+public class Board {
+    @Id //id를 pk값으로 사용함을 명시
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id; //아이디
+    @NotNull
+    @Size(min=2, max=30, message = "제목은 2자이상 30자 이하입니다.") //2글자 이상 30글자 이하 제한
+    private String title; //제목
+    private String content; //내용
+}
